57b21bd21fcbfec1c645fbd6a3597e88ff4d8383
fazilh94 <fazilh94@gmail.com>
2022-10-17 02:11:47 -0400

    Streamlined page title accessibility

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,15 +7,18 @@ import { RouteAccessGuard } from './route-access.guard';
 const routes: Routes = [
   {
     path:'employeeform',
-    component:EmployeeFormComponent
+    component:EmployeeFormComponent,
+    title: 'Employee Portal - Onboarding'
   },
   {
     path:'employeedetails', loadComponent:()=> import('./employee-details/employee-details.component')
-    .then(mod=>mod.EmployeeDetailsComponent)
+    .then(mod=>mod.EmployeeDetailsComponent),
+    title: 'Employee Portal - List'
   },
   {
     path:'employeeportfolio', loadComponent:()=> import('./employee-portfolio/employee-portfolio.component')
-    .then(mod=>mod.EmployeePortfolioComponent), canActivate:[RouteAccessGuard]
+    .then(mod=>mod.EmployeePortfolioComponent), canActivate:[RouteAccessGuard],
+    title: 'Employee Portal - Portfolio'
   },
   {
     path:'', redirectTo:'/employeeform', pathMatch:"full"
